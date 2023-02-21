4b366f5aac5328d5d1d26a9eef62c7a827718ee9
的出道的书法家 <98867110+aass123as@users.noreply.github.com>
2023-02-21 14:58:43 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# wang
-基于vue element-plus 构建的购物网站
+# yanxuank
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'less',
+      patterns: [
+        path.join(__dirname, './src/assets/style/variables.less'),
+        path.join(__dirname, './src/assets/style/mixins.less')
+      ]
+    }
+  }
+})
